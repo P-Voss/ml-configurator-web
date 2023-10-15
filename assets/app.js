72d9fddff2b9eb7b3be54e2286f6be3385ad0a73
Philipp <voss.ph@web.de>
2023-10-15 12:35:55 +0200

    styling and frontend setup

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+
+import './styles/app.scss';
+// import 'bootstrap';
+// import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+// import bsCustomFileInput from 'bs-custom-file-input';
+
+// bsCustomFileInput.init();
+
