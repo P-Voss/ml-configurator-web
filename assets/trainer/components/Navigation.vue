f8ac4a75fa47fb1d06bec31890f8b8364f4a2109
Philipp <voss.ph@web.de>
2023-10-19 20:23:59 +0200

    responsive navigation in training component

--- assets/trainer/components/Navigation.vue
+++ assets/trainer/components/Navigation.vue
@@ -1,6 +1,57 @@
 <template>
     <div>
-        <ul class="nav nav-pills mb-3 justify-content-center nav-fill" id="trainer-tab" role="tablist">
+        <ul class="nav nav-pills mb-3 justify-content-center nav-fill d-none d-lg-flex" id="trainer-tab-lg" role="tablist">
+            <li class="nav-item" role="presentation">
+                <a class="nav-link bg-success text-light"
+                   :href="configuratorUrl" role="tab"
+                >
+                    Konfigurator
+                </a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link"
+                   :class="{'active': currentStep === 'DATA_PREP', 'bg-success': initStepCompleted, 'text-light': initStepCompleted}"
+                   @click="() => {
+                       this.$emit('set-step', 'DATA_PREP')
+                       currentStep = 'DATA_PREP'
+                   }"
+                   id="trainer-data-prep-tab-lg" data-bs-toggle="pill" href="#trainer-data-prep" role="tab" aria-controls="trainer-data-prep" aria-selected="true">
+                    Datenvorbereitung
+                </a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link"
+                   :class="{'active': currentStep === 'HYPERPARAMETER', 'disabled' : !initStepCompleted}"
+                   @click="() => {
+                       this.$emit('set-step', 'HYPERPARAMETER')
+                       currentStep = 'HYPERPARAMETER'
+                   }"
+                   id="trainer-hyperparameter-tab-lg" data-bs-toggle="pill" href="#trainer-hyperparameter" role="tab" aria-controls="trainer-hyperparameter" aria-selected="false">
+                    Hyperparameter
+                </a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link"
+                   :class="{'active': currentStep === 'TRAINING', 'disabled' : !initStepCompleted}"
+                   @click="() => {
+                       this.$emit('set-step', 'TRAINING')
+                       currentStep = 'TRAINING'
+                   }"
+                   id="trainer-training-tab-lg" data-bs-toggle="pill" href="#trainer-training" role="tab" aria-controls="trainer-training" aria-selected="false">
+                    Training
+                </a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link"
+                   :class="{'disabled' : !initStepCompleted || !hyperparameterStepCompleted || !trainingStepCompleted}"
+                   id="trainer-execute-tab-lg" :href="executorUrl" role="tab" aria-controls="trainer-execute" aria-selected="false">
+                    Anwendung
+                </a>
+            </li>
+        </ul>
+
+
+        <ul class="nav nav-pills mb-3 justify-content-center flex-column nav-fill d-flex d-lg-none gap-2" id="trainer-tab" role="tablist">
             <li class="nav-item" role="presentation">
                 <a class="nav-link bg-success text-light"
                    id="trainer-data-prep-tab" :href="configuratorUrl" role="tab" aria-controls="trainer-data-prep"
@@ -44,7 +95,7 @@
             <li class="nav-item" role="presentation">
                 <a class="nav-link"
                    :class="{'disabled' : !initStepCompleted || !hyperparameterStepCompleted || !trainingStepCompleted}"
-                   id="trainer-training-tab" :href="executorUrl" role="tab" aria-controls="trainer-training" aria-selected="false">
+                   id="trainer-execute-tab" :href="executorUrl" role="tab" aria-controls="trainer-execute" aria-selected="false">
                     Anwendung
                 </a>
             </li>
